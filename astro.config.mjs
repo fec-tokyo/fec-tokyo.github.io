206aa1953b50811c20ce19c7f92074638a69d4fa
mmrakt <mimuraakkun3@gmail.com>
2025-08-25 12:41:31 +0900

    feat: KVのSP実装

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,21 @@
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	image: {
+		responsiveStyles: true,
+		layout: "constrained",
+	},
+	vite: {
+		css: {
+			preprocessorOptions: {
+				scss: {
+					additionalData: `
+                    @use "@/css/_mixins.scss" as mixin;
+                    @use "@/css/_vars.scss" as var;
+                    `,
+				},
+			},
+		},
+	},
+});
