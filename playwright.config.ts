d93acf54b86d245778e99230c307b623d8d2fde6
mmrakt <mimuraakkun3@gmail.com>
2025-08-29 11:24:59 +0900

    fix: 全体的なデザインの調整、リファクタ

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,36 +1,36 @@
-import { defineConfig, devices } from '@playwright/test';
+import { defineConfig, devices } from "@playwright/test";
 
 export default defineConfig({
-  testMatch: '**/*.test.ts',
+  testMatch: "**/*.test.ts",
   projects: [
     {
-      name: 'Chromium Desktop',
+      name: "Chromium Desktop",
       use: {
-        ...devices['Desktop Chrome']
+        ...devices["Desktop Chrome"],
       },
     },
     {
-      name: 'Chromium Android',
+      name: "Chromium Android",
       use: {
-        ...devices['Pixel 7']
+        ...devices["Pixel 7"],
       },
     },
     {
-      name: 'WebKit Desktop',
+      name: "WebKit Desktop",
       use: {
-        ...devices['Desktop Safari']
+        ...devices["Desktop Safari"],
       },
     },
     {
-      name: 'WebKit iOS',
+      name: "WebKit iOS",
       use: {
-        ...devices['iPhone 14']
+        ...devices["iPhone 14"],
       },
     },
   ],
   webServer: {
-    command: 'npm run serve',
-    url: 'http://localhost:3000',
+    command: "npm run serve",
+    url: "http://localhost:3000",
     reuseExistingServer: !process.env.CI,
-  }
+  },
 });
