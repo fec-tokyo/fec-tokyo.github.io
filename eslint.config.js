7530802f150e66417de29fa91ab0cc4e9b397d8a
mmrakt <mimuraakkun3@gmail.com>
2025-08-29 11:18:26 +0900

    chore: biomeで意図しないエラーを検知してしまうためeslintに入れ替え

---
+++ eslint.config.js
@@ -0,0 +1,73 @@
+import js from "@eslint/js";
+import tseslint from "@typescript-eslint/eslint-plugin";
+import tsparser from "@typescript-eslint/parser";
+import astro from "eslint-plugin-astro";
+import globals from "globals";
+
+export default [
+  js.configs.recommended,
+
+  {
+    files: ["**/*.{js,ts}"],
+    plugins: {
+      "@typescript-eslint": tseslint,
+    },
+    languageOptions: {
+      parser: tsparser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+      },
+      globals: {
+        ...globals.node,
+        ...globals.browser,
+        NodeListOf: "readonly",
+        HTMLElement: "readonly",
+        HTMLButtonElement: "readonly",
+      },
+    },
+    rules: {
+      ...tseslint.configs.recommended.rules,
+
+      indent: ["error", 2],
+      quotes: ["error", "double"],
+      semi: ["error", "always"],
+      "prefer-const": "error",
+      "no-var": "error",
+      "object-shorthand": "error",
+      "prefer-arrow-callback": "error",
+    },
+  },
+  ...astro.configs["flat/recommended"],
+  {
+    files: ["**/*.astro"],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        NodeListOf: "readonly",
+        HTMLElement: "readonly",
+        HTMLButtonElement: "readonly",
+      },
+    },
+    rules: {
+      indent: ["error", 2],
+      quotes: ["error", "double"],
+    },
+  },
+  {
+    files: ["eslint.config.js"],
+    rules: {
+      indent: "off",
+    },
+  },
+  {
+    ignores: [
+      "node_modules/**",
+      "dist/**",
+      "build/**",
+      ".git/**",
+      ".astro/**",
+      ".claude/utils/**",
+    ],
+  },
+];
